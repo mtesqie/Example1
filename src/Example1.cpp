ab58aeb1310fc6e0f15eb92aa398863c97c9ce06
malak <mtesqie@ryerson.ca>
2015-10-21 17:08:15 -0400

    demo lecture for using githp in eclipse

---
+++ src/Example1.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : Example1.cpp
+// Author      : Malak
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout<< "my  name is malak"<< endl;
+	cout<<"hello";
+	return 0;
+}
